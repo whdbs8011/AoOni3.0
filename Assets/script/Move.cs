024026f0ba1f69c2d390c68164023fcc05a810c0
joyun <choyoon0811@gmail.com>
2024-12-19 12:19:16 +0900

    Add
    걷는 스크립트 대폭 수정

---
+++ Assets/script/Move.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float Speed;
+    private Rigidbody2D rigid;
+    private float h;
+    private float v;
+    private bool isHorizonMove;
+    
+    private Animation ani;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        ani = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+        if (hDown)
+        {
+            isHorizonMove = true;
+        }
+        else if (vDown)
+        {
+            isHorizonMove = false;
+        }
+        else if (hUp || vUp)
+        {
+            isHorizonMove = h != 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec * Speed;
+    }
+}
